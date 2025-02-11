38caaab20cc702d0aa0d85b45c162a2aa6414b46
Lee Dogeon <dev.moreal@gmail.com>
2025-02-11 15:06:04 +0900

    Initialize backend project

---
+++ backend/finds.team/settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "finds_team"
---
+++ backend/finds.team/src/test/kotlin/dev/moreal/finds_team/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.moreal.finds_team
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
